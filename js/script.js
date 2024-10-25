b388de37ec8c49c9652132c5e7b1158e74bb35b4
Danielle Brown <hello@danielleallyssa.com>
2024-10-25 14:52:24 -0400

    cleaned up animation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
 const productItems = document.querySelectorAll(".product-tile__item");
+const gridHLines = document.querySelectorAll("section > .line.hor");
 const gridVLines = document.querySelectorAll(".product-tile__item .line.vert");
 
-// const start = gsap.timeline({ defaults: { ease: "power4.out" } });
+const start = gsap.timeline({
+  defaults: { ease: "power4.out", duration: 0.75 },
+});
 
-start.add([
-  gsap.to(gridVLines, {
+start
+  .to(gridHLines, {
+    width: "100%",
+  })
+  .to(gridVLines, {
     height: "100%",
-  }),
-]);
+  })
+  .from(
+    "header",
+    {
+      y: "-100%",
+    },
+    "<.125"
+  )
+  .from(
+    "h2",
+    {
+      opacity: 0,
+    },
+    "<"
+  );
 
 productItems.forEach((item) => {
-  // const tl = gsap.timeline({ defaults: { ease: "power4.out" } });
+  const tl = gsap.timeline({ defaults: { ease: "power4.out" } });
 
   const title = item.querySelector(".product-tile__title");
   const button = item.querySelector("a.btn");
   const revealBox = item.querySelectorAll(".reveal");
   const revealText = item.querySelectorAll(".reveal span");
+  const img = document.querySelectorAll("img");
 
   tl.from(title, {
     opacity: 0,
@@ -29,7 +49,24 @@ productItems.forEach((item) => {
     .from(revealText, {
       yPercent: 100,
       stagger: 0.1,
-    });
+    })
+    .fromTo(
+      img,
+      {
+        autoAlpha: 0,
+        scale: 0.95,
+      },
+      {
+        autoAlpha: 1,
+        scale: 1,
+        duration: 1,
+      },
+      "<.25"
+    );
+
+  // img.onload = () => {
+
+  // };
 
   tl.play();
 
