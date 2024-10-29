0145c5f7ab790d413bc69f6b7e73adaffd9ec163
Danielle Brown <hello@danielleallyssa.com>
2024-10-29 00:18:01 -0400

    Completed animation

--- js/script.js
+++ js/script.js
@@ -9,64 +9,88 @@ class App {
 
   _getElements() {
     return {
-      h_Line: document.querySelector("section > .line.hor"),
-      v_Lines: [...document.querySelectorAll(".product-tile__item .line.vert")],
+      hLine: document.querySelector("section > .line.hor"),
+      vLines: [...document.querySelectorAll(".product-tile__item .line.vert")],
       revealConts: [
         ...document.querySelectorAll(".product-tile__item .reveal"),
       ],
       collTitle: document.querySelector("h2"),
-      prodTitle: [...document.querySelectorAll("h3.product-tile__title")],
+      prodTitles: [...document.querySelectorAll("h3.product-tile__title")],
       prodDetails: [
         ...document.querySelectorAll(".product-tile__item .reveal span"),
       ],
       prodLinks: [...document.querySelectorAll("a.btn")],
+      prodImages: [
+        ...document.querySelectorAll(".product-tile__image-wrapper img"),
+      ],
+      header: document.querySelector("header"),
     };
   }
 
   _init() {
     this._setInitialStates();
 
-    let main = gsap.timeline();
-    main.add(this._animateLines()).add(this._revealUI(), "<1");
-    GSDevTools.create({ animation: main });
+    gsap.timeline().add(this._animateHeaderLines()).add(this._revealUI(), "<1");
+
+    // GSDevTools.create({ animation: main });
   }
 
   _setInitialStates() {
-    gsap.set(this.elements.h_Line, {
+    const {
+      prodTitles,
+      prodLinks,
+      prodImages,
+      prodDetails,
+      collTitle,
+      hLine,
+      vLines,
+      revealConts,
+      header,
+    } = this.elements;
+
+    gsap.set(header, { y: "-105%" });
+    gsap.set(hLine, {
       clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)",
     });
-    gsap.set(this.elements.v_Lines, { height: 0 });
-    gsap.set(this.elements.revealConts, {
+    gsap.set(vLines, { height: 0 });
+    gsap.set(revealConts, {
       clipPath: "polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)",
     });
-    gsap.set(
-      [
-        this.elements.collTitle,
-        this.elements.prodTitle,
-        this.elements.prodLinks,
-      ],
-      {
-        autoAlpha: 0,
-        y: -10,
-      }
-    );
-    gsap.set(this.elements.prodDetails, {
+    gsap.set(collTitle, {
+      autoAlpha: 0,
+      y: -10,
+    });
+    gsap.set([prodTitles, prodLinks], {
+      autoAlpha: 0,
+      y: "50%",
+    });
+    gsap.set([prodImages], {
+      autoAlpha: 0,
+      y: "10%",
+    });
+    gsap.set(prodDetails, {
       y: "105%",
     });
   }
 
-  _animateLines() {
+  _animateHeaderLines() {
+    const { hLine, vLines, revealConts, header } = this.elements;
     let tl = gsap.timeline({
       delay: 0.25,
       defaults: { ease: "power4.inOut", duration: 1 },
     });
 
-    tl.to(this.elements.h_Line, {
-      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    })
-      .to(this.elements.v_Lines, { height: "100%" }, "<")
+    tl.to(header, { y: 0 })
       .to(
-        this.elements.revealConts,
+        hLine,
+        {
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        },
+        "<.05"
+      )
+      .to(vLines, { height: "100%" }, "<")
+      .to(
+        revealConts,
         {
           clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
           stagger: 0.025,
@@ -78,77 +102,34 @@ class App {
   }
 
   _revealUI() {
-    const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-    tl.to(
-      [
-        this.elements.collTitle,
-        this.elements.prodTitle,
-        this.elements.prodLinks,
-      ],
-      {
-        autoAlpha: 1,
-        y: 0,
-      }
-    ).to(
-      this.elements.prodDetails,
-      {
-        y: 0,
-        stagger: 0.025,
-      },
-      "<.5"
-    );
+    const { prodTitles, prodLinks, prodImages, prodDetails, collTitle } =
+      this.elements;
+    const tl = gsap.timeline({
+      defaults: { ease: "power4.out", duration: 0.75 },
+    });
+    tl.to([prodTitles, prodLinks, prodImages], {
+      autoAlpha: 1,
+      y: 0,
+    })
+      .to(
+        prodDetails,
+        {
+          y: 0,
+          delay: 0.1,
+        },
+        "<"
+      )
+      .to(
+        [collTitle],
+        {
+          autoAlpha: 1,
+          y: 0,
+        },
+        "-=.25"
+      );
 
     return tl;
   }
 }
 
 new App();
-
-// function initItem() {
-//   productItems.forEach((item) => {
-//     const itemEl = {
-//       contentWrap: item.querySelector(".product-tile__content-wrapper"),
-//       imageWrap: item.querySelector(".product-tile__image-wrapper"),
-//       title: item.querySelector(".product-tile__title"),
-//       link: item.querySelector("a.btn"),
-//       reveal: item.querySelectorAll(".reveal"),
-//       revealText: item.querySelectorAll(".reveal span"),
-//       img: document.querySelectorAll("img"),
-//     };
-
-//     const tl = gsap
-//       .timeline({
-//         defaults: { ease: "power4.out" },
-//         paused: true,
-//       })
-//       .to(itemEl.contentWrap, {
-//         autoAlpha: 1,
-//         duration: 0.25,
-//       })
-//       .to(
-//         itemEl.imageWrap,
-//         {
-//           autoAlpha: 1,
-//         },
-//         "<"
-//       )
-//       .fromTo(
-//         itemEl.img,
-//         {
-//           autoAlpha: 0,
-//           y: 50,
-//         },
-//         {
-//           autoAlpha: 1,
-//           y: 0,
-//           duration: 1,
-//         },
-//         "<.25"
-//       )
-//         {
-//           autoAlpha: 1,
-//           y: 0,
-//           duration: 1,
-//         },
-//         "-=.25"
-//       );
